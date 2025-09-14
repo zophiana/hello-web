9f0c23f270eb44eb782c88f8e38c7a5b36383e42
zophiana <53372671+zophiana@users.noreply.github.com>
2025-09-14 16:01:28 +0200

    add prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
+
+export default config;
